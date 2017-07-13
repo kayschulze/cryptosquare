0543b3753177001860c60e23b5d0d814c169cfa3
Kim Schulze <kimberlykayschulze@gmail.com>
2017-07-13 16:09:14 -0700

    complete spec 2 by outputting a 4-digit word using a cryptosquare.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
+var messageToArray = function(input) {
+  return input.split("");
+}
+
 var encryptInput = function(message) {
-  return message;
+  messageArray = messageToArray(message);
+  var encryptedOutput = [];
+
+  encryptedOutput[0] = messageArray[0];
+  encryptedOutput[1] = messageArray[2];
+  encryptedOutput[2] = messageArray[1];
+  encryptedOutput[3] = messageArray[3];
+
+  return encryptedOutput.join("");
 };
 
 // Main function calls encryptInput() method and displays answer
